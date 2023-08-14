92a936568b71b0e05d9109af6d62ac10caec9e72
El Heroe <116875322+Liya-23@users.noreply.github.com>
2023-08-14 14:19:22 +0200

    Update nml.py

--- nml.py
+++ nml.py
@@ -2,7 +2,7 @@ from tkinter import *
 from datetime import date
 from tkinter import filedialog
 from tkinter import messagebox
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk  
 import os
 from tkinter.ttk import Combobox
 import openpyxl, xlrd
